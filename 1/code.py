206b897efdcc96e33f271d4152ef045882eeb212
Alex-Merrill <aomerrill98@gmail.com>
2023-12-02 05:16:42 -0500

    days 1 and 2

---
+++ 1/code.py
@@ -0,0 +1,72 @@
+import re
+
+
+def part1(lines):
+    # read input
+    sum = 0
+    for line in lines:
+        n = ""
+        l = 0
+        while l < len(line):
+            if line[l].isnumeric():
+                n += line[l]
+                break
+            l += 1
+        
+        r = len(line) - 1
+        while r >= l:
+            if line[r].isnumeric():
+                n += line[r]
+                break
+            r -= 1
+        sum += int(n)
+    
+    return sum
+
+
+def part2(lines):
+    num_dict = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+        "1": "1",
+        "2": "2",
+        "3": "3",
+        "4": "4",
+        "5": "5",
+        "6": "6",
+        "7": "7",
+        "8": "8",
+        "9": "9",
+    }
+    sum = 0
+    for l in lines:
+        nums = []
+        for k, v in num_dict.items():
+            for match in re.finditer(k, l):
+                if match.start() != -1:
+                    nums.append((match.start(), v))
+        nums.sort()
+        calibration = nums[0][1] + nums[-1][1]
+
+        sum += int(calibration)
+    
+    return sum
+
+
+def main():
+    lines = []
+    with open("input.txt") as f:
+        for line in f:
+            lines.append(line)
+    print(part1(lines))
+    print(part2(lines))
+
+
+main()
---
+++ 2/code.py
@@ -0,0 +1,65 @@
+import re
+import collections
+
+def part1(lines):
+    max_cubes = {"red": 12, "green": 13, "blue": 14}
+    sum = 0
+    for l in lines:
+        game, game_data = l.split(":") # "Game x:", "3 blue, 4 red; 1 green"
+        game_id = int(game.split()[1])
+        rounds = game_data.split(";") # ["3 blue, 4 red", "1 green"]
+        max_cubes_drawn = collections.defaultdict(int)
+        game_invalid = False
+        for r in rounds:
+            cube_draws = r.split(",") # ["3 blue", "4 red"]
+            for draw in cube_draws:
+                num_cubes, color = draw.split()
+                max_cubes_drawn[color] = max(max_cubes_drawn[color], int(num_cubes))
+                if max_cubes_drawn[color] > max_cubes[color]:
+                    game_invalid = True
+        
+        if game_invalid:
+            continue
+        
+        sum += game_id
+    
+    return sum
+            
+
+
+def part2(lines):
+    sum = 0
+    for l in lines:
+        game, game_data = l.split(":") # "Game x:", "3 blue, 4 red; 1 green"
+        game_id = int(game.split()[1])
+        rounds = game_data.split(";") # ["3 blue, 4 red", "1 green"]
+        max_cubes_drawn = collections.defaultdict(int)
+        game_invalid = False
+        for r in rounds:
+            cube_draws = r.split(",") # ["3 blue", "4 red"]
+            for draw in cube_draws:
+                num_cubes, color = draw.split()
+                max_cubes_drawn[color] = max(max_cubes_drawn[color], int(num_cubes))
+        
+        power = 1
+        for v in max_cubes_drawn.values():
+            power *= v
+
+        sum += power
+    
+    return sum
+
+
+
+
+
+def main():
+    lines = []
+    with open("input.txt") as f:
+        for line in f:
+            lines.append(line)
+    print(part1(lines))
+    print(part2(lines))
+
+
+main()
