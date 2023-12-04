d2cd06d1c587edb293739e9fb111948878ccc39e
Alex-Merrill <aomerrill98@gmail.com>
2023-12-04 00:22:06 -0500

    ...

--- 4/code.py
+++ 4/code.py
@@ -16,18 +16,17 @@ def part2(lines):
     total_scratchcards = 0
     copies = [1] * len(lines)
     for i, line in enumerate(lines):
-        while copies[i] > 0:
-            game_id, game_data = line.split(":")
-            _, game_id = game_id.split()
-            winning_nums, my_nums = game_data.split("|")
-            winning_nums = set(winning_nums.split())
-            my_nums = set(my_nums.split())
-            num_wins = len(my_nums.intersection(winning_nums))
-            for j in range(i+1, i+1+num_wins):
-                copies[j] += 1
-            
-            copies[i] -= 1
-            total_scratchcards += 1
+        game_id, game_data = line.split(":")
+        _, game_id = game_id.split()
+        winning_nums, my_nums = game_data.split("|")
+        winning_nums = set(winning_nums.split())
+        my_nums = set(my_nums.split())
+        num_wins = len(my_nums.intersection(winning_nums))
+
+        for j in range(i+1, i+1+num_wins):
+            copies[j] += copies[i]
+        
+        total_scratchcards += copies[i]
 
 
     return total_scratchcards
