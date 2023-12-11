725dfeb42b5e2d625939db2bda81d916c9c65b76
Alex-Merrill <aomerrill98@gmail.com>
2023-12-11 01:40:16 -0500

    day 11

--- 10/code.py
+++ 10/code.py
@@ -1,61 +1,62 @@
 import collections
 
+
 def part1(graph, start):
-    path = { # tells us what directions to travel based on current pipe
-        '|': [(1,0), (-1, 0)],
-        '-': [(0,1), (0,-1)],
-        'L': [(-1,0), (0,1)],
-        'J': [(-1,0), (0,-1)],
-        '7': [(1,0), (0,-1)],
-        'F': [(1,0), (0,1)],
+    path = {  # tells us what directions to travel based on current pipe
+        "|": [(1, 0), (-1, 0)],
+        "-": [(0, 1), (0, -1)],
+        "L": [(-1, 0), (0, 1)],
+        "J": [(-1, 0), (0, -1)],
+        "7": [(1, 0), (0, -1)],
+        "F": [(1, 0), (0, 1)],
     }
-    cleaned_graph = [['.']*len(graph[0]) for _ in range(len(graph))] 
+    cleaned_graph = [["."] * len(graph[0]) for _ in range(len(graph))]
     q = collections.deque([(start[0], start[1], 0)])
     visited = set(start)
-    mx_d = (start[0],start[1],0)
+    mx_d = (start[0], start[1], 0)
     while q:
-        i,j,d = q.popleft()
+        i, j, d = q.popleft()
         c = graph[i][j]
         cleaned_graph[i][j] = c
 
         if d > mx_d[2]:
-            mx_d = (i,j,d)
+            mx_d = (i, j, d)
 
         for di, dj in path[c]:
             ni, nj = i + di, j + dj
             if (ni, nj) in visited:
                 continue
-            visited.add((ni,nj))
-            q.append((ni, nj, d+1))
+            visited.add((ni, nj))
+            q.append((ni, nj, d + 1))
 
     return mx_d[2], cleaned_graph
 
 
 def replace_start(graph, start):
-    i,j = start
+    i, j = start
     start = 0
-    if graph[i][j+1] in ['-', 'J', '7']:
+    if graph[i][j + 1] in ["-", "J", "7"]:
         start += 1
-    if graph[i][j-1] in ['-', 'L', 'F']:
+    if graph[i][j - 1] in ["-", "L", "F"]:
         start += 2
-    if graph[i+1][j] in ['|', 'L', 'J']:
+    if graph[i + 1][j] in ["|", "L", "J"]:
         start += 3
-    if graph[i-1][j] in ['|', 'F', '7']:
+    if graph[i - 1][j] in ["|", "F", "7"]:
         start += 5
 
     if start == 3:
-        graph[i][j] == '-'
+        graph[i][j] == "-"
     elif start == 8:
-        graph [i][j] = '|'
+        graph[i][j] = "|"
     elif start == 6:
-        graph [i][j] = 'L'
+        graph[i][j] = "L"
     elif start == 7:
-        graph [i][j] = 'J'
+        graph[i][j] = "J"
     elif start == 5:
-        graph [i][j] = '7'
+        graph[i][j] = "7"
     elif start == 4:
-        graph [i][j] = 'F'
-    
+        graph[i][j] = "F"
+
     return graph
 
 
@@ -77,41 +78,43 @@ def part2(graph):
     for i in range(len(graph)):
         boundaries = 0
         for j in range(len(graph[0])):
-            if graph[i][j] in ['|', 'J', 'L']:
+            if graph[i][j] in ["|", "J", "L"]:
                 boundaries += 1
-            elif graph[i][j] == '.' and boundaries % 2 == 1:
+            elif graph[i][j] == "." and boundaries % 2 == 1:
                 num_inside += 1
-                graph[i][j] = 'I'
-            elif graph[i][j] == '.':
-                graph[i][j] = 'O'
-    
+                graph[i][j] = "I"
+            elif graph[i][j] == ".":
+                graph[i][j] = "O"
+
     print_graph(graph)
     return num_inside
 
+
 def print_graph(graph):
     d = {
-        '-' : '─',
-        '|' : '│',
-        'L' : '╰',
-        'J' : '╯',
-        '7' : '╮',
-        'F' : '╭',
-        'I' : '█',
-        'O' : '░',
-        '.' : ' ',
+        "-": "─",
+        "|": "│",
+        "L": "╰",
+        "J": "╯",
+        "7": "╮",
+        "F": "╭",
+        "I": "█",
+        "O": "░",
+        ".": " ",
     }
     for line in graph:
-        print(''.join([d[c] for c in line]))
+        print("".join([d[c] for c in line]))
+
 
 def main():
     lines = open("input.txt").read().strip().splitlines()
     graph = [[] for _ in range(len(lines))]
-    start = (-1,-1)
+    start = (-1, -1)
     for i, line in enumerate(lines):
         for j, c in enumerate(line):
             graph[i].append(c)
             if c == "S":
-                start = (i,j)
+                start = (i, j)
 
     graph = replace_start(graph, start)
     print_graph(graph)
---
+++ 11/code.py
@@ -0,0 +1,51 @@
+def solve(gals, rows_no_gal, cols_no_gal, scale):
+    sum_dist = 0
+    for i in range(len(gals)):
+        for j in range(i + 1, len(gals)):
+            num_rows_btwn = len(
+                [x for x in rows_no_gal if between(x, gals[i][0], gals[j][0])]
+            )
+            num_cols_btwn = len(
+                [x for x in cols_no_gal if between(x, gals[i][1], gals[j][1])]
+            )
+
+            sum_dist += (
+                man_dist(gals[i], gals[j])
+                + num_rows_btwn * (scale - 1)
+                + num_cols_btwn * (scale - 1)
+            )
+
+    return sum_dist
+
+
+def man_dist(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+def between(val, b1, b2):
+    return min(b1, b2) < val < max(b1, b2)
+
+
+def main():
+    inpt = open("input.txt").read().strip().split("\n")
+    graph = [[c for c in line] for line in inpt]
+
+    gals = []
+    rows_no_gal = [True] * len(graph)
+    cols_no_gal = [True] * len(graph[0])
+    for i in range(len(graph)):
+        for j in range(len(graph[0])):
+            if graph[i][j] == "#":
+                gals.append((i, j))
+                rows_no_gal[i] = False
+                cols_no_gal[j] = False
+    rows_no_gal = [i for i in range(len(rows_no_gal)) if rows_no_gal[i]]
+    cols_no_gal = [i for i in range(len(cols_no_gal)) if cols_no_gal[i]]
+
+    dist = solve(gals, rows_no_gal, cols_no_gal, 2)
+    print(f"part1: {dist}")
+    dist = solve(gals, rows_no_gal, cols_no_gal, 1000000)
+    print(f"part2: {dist}")
+
+
+main()
