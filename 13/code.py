c5ebf3ce4942c62815357e36c541c6098c051ec8
Alex-Merrill <aomerrill98@gmail.com>
2023-12-13 02:20:27 -0500

    d13 cleanup

--- 13/code.py
+++ 13/code.py
@@ -1,25 +1,15 @@
 def row_sym(pattern, diffs_req=0):
     for i in range(len(pattern) - 1):
-        first, second = i, i + 1
-        num_diffs = 0
-        while first >= 0 and second < len(pattern):
-            diffs = num_diff(pattern[first], pattern[second])
-            if diffs > 1:
+        total_diffs = 0
+        for first, second in zip(pattern[i::-1], pattern[i + 1 :]):
+            total_diffs += sum([f != s for f, s in zip(first, second)])
+            if total_diffs > diffs_req:
                 break
-            elif diffs == 1:
-                num_diffs += 1
 
-            first -= 1
-            second += 1
+        if total_diffs == diffs_req:
+            return i + 1
 
-        if num_diffs == diffs_req and (first < 0 or second >= len(pattern)):
-            return i
-
-    return -1
-
-
-def num_diff(s, r):
-    return sum([s != r for s, r in zip(s, r)])
+    return 0
 
 
 def main():
@@ -36,8 +26,8 @@ def main():
         c_sym1 = row_sym(pattern)
         c_sym2 = row_sym(pattern, diffs_req=1)
 
-        p1_ans += c_sym1 + 1 + (r_sym1 + 1) * 100
-        p2_ans += c_sym2 + 1 + (r_sym2 + 1) * 100
+        p1_ans += c_sym1 + r_sym1 * 100
+        p2_ans += c_sym2 + r_sym2 * 100
 
     print(f"part1: {p1_ans}")
     print(f"part2: {p2_ans}")
