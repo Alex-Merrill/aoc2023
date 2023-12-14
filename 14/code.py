2afe263dacb104d2d6776feac08cb3d03eae4c1e
Alex-Merrill <aomerrill98@gmail.com>
2023-12-14 03:33:51 -0500

    optimize

--- 14/code.py
+++ 14/code.py
@@ -1,37 +1,67 @@
-def push_north(rows):
-    """
-    pushes all rocks north
-    """
-    for i in range(1, len(rows)):
-        for j in range(len(rows[0])):
-            if rows[i][j] != "O":
-                continue
-            curr_row = i
-            while curr_row >= 1 and rows[curr_row - 1][j] == ".":
-                rows[curr_row - 1][j] = "O"
-                rows[curr_row][j] = "."
-                curr_row = curr_row - 1
+import time
+
+
+def push_north_south(rows, dir):
+    row_start = 0 if dir == "north" else len(rows) - 1
+    row_end = len(rows) if dir == "north" else -1
+    row_step = 1 if dir == "north" else -1
+    ob_start = -1 if dir == "north" else len(rows)
+    for j in range(len(rows[0])):
+        ob_row = ob_start
+        for i in range(row_start, row_end, row_step):
+            if rows[i][j] == "#":
+                ob_row = i
+            if rows[i][j] == "O":
+                rows[i][j] = "."
+                rows[ob_row + row_step][j] = "O"
+                ob_row = ob_row + row_step
 
     return rows
 
 
+def push_east_west(rows, dir):
+    col_start = 0 if dir == "west" else len(rows[0]) - 1
+    col_end = len(rows[0]) if dir == "west" else -1
+    col_step = 1 if dir == "west" else -1
+    ob_start = -1 if dir == "west" else len(rows[0])
+    for i in range(len(rows)):
+        ob_col = ob_start
+        for j in range(col_start, col_end, col_step):
+            if rows[i][j] == "#":
+                ob_col = j
+            if rows[i][j] == "O":
+                rows[i][j] = "."
+                rows[i][ob_col + col_step] = "O"
+                ob_col = ob_col + col_step
+
+
+def push_rocks(rows, dir):
+    if dir in ["north", "south"]:
+        push_north_south(rows, dir)
+    else:
+        push_east_west(rows, dir)
+
+
+def part1(rows):
+    push_rocks(rows, "north")
+    return calc_load(rows)
+
+
 def part2(rows, cycles):
     """
-    for each cycle, push rocks north, transpose, repeat, until one
-    cycle has completed. We store the grid state and cycle iteration
-    to look for a point in which we have a repeated grid state.
-    Since each cycle iteration does the same operations, we know we have
-    now found a loop. Now we simply calculate which iteration in the loop
-    the grid will be at after "cycles" iterations and return that grid state
+    for each cycle, push rocks in each direction. Then store the grid state
+    and the number of cycles it took in order to find a repeating grid state.
+    Since each cycle iteration does the same operations, we know we have now
+    found a loop. Now we simply calculate which iteration in the loop the
+    grid will be at after "cycles" iterations and return that grid state
     """
     g_to_cycle = dict()
     cycle_to_g = dict()
     cycle_start = None
     period = None
     for i in range(1, cycles + 1):
-        for _ in range(4):
-            rows = push_north(rows)
-            rows = transpose(rows)
+        for dir in ["north", "west", "south", "east"]:
+            push_rocks(rows, dir)
 
         key = "\n".join(["".join(r) for r in rows])
         if key in g_to_cycle:
@@ -43,7 +73,7 @@ def part2(rows, cycles):
         cycle_to_g[i] = key
 
     cycles = cycle_start + (cycles - cycle_start) % period
-    return cycle_to_g[cycles].split("\n")
+    return calc_load(cycle_to_g[cycles].split("\n"))
 
 
 def calc_load(g):
@@ -64,12 +94,13 @@ def main():
     inpt = open("input.txt").read().strip().splitlines()
     rows = [list(row) for row in inpt]
 
-    end_state = push_north([[c for c in row] for row in rows])
-    print(f"part1: {calc_load(end_state)}")
+    t = time.perf_counter()
+    p1_ans = part1([[c for c in row] for row in rows])
+    print(f"part 1: {p1_ans} in {time.perf_counter() - t} seconds")
 
-    cycles = 1000000000
-    end_state = part2([[c for c in row] for row in rows], cycles)
-    print(f"part2: {calc_load(end_state)}")
+    t = time.perf_counter()
+    p2_ans = part2([[c for c in row] for row in rows], 1000000000)
+    print(f"part2: {p2_ans} in {time.perf_counter() - t} seconds")
 
 
 main()
